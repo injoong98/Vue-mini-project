f862003db5c37b526996a02ac3e6deb9a65dd32e
injoong98 <qwe_asd13579@naver.com>
2022-11-04 15:28:02 +0900

    결과페이지 디자인

--- src/components/ContentResult.vue
+++ src/components/ContentResult.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
-    <h2>결과 입니다.</h2>
+    <img
+      src="../assets/resultBack.jpg"
+      style="
+        position: absolute;
+        z-index: -100;
+        top: 0px;
+        left: 0px;
+        width: 100vw;
+      "
+    />
+    <div style="margin-top: 50px">
+      <h2>준용쿠키가 발송되었습니다.</h2>
+      <p>이메일을 확인해보세요</p>
 
-    <p>{{ $route.params.message }}</p>
-
-    <a id="kakaotalk-sharing-btn" @click="kakaoLink" href="javascript:;">
-      <img
-        src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
-        alt="카카오톡 공유 보내기 버튼"
-      />
-    </a>
+      <a id="kakaotalk-sharing-btn" @click="kakaoLink" href="javascript:;">
+        <img
+          src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
+          alt="카카오톡 공유 보내기 버튼"
+        />
+      </a>
+      <div>카카오톡으로 공유도 할 수 있어요</div>
+    </div>
   </div>
 </template>
 
@@ -37,4 +49,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style scope>
+* {
+  color: white;
+}
+</style>

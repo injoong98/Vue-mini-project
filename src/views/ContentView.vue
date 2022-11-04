9eb9705af2be253af085585d5d5ed336b97271d6
injoong98 <qwe_asd13579@naver.com>
2022-11-04 14:33:54 +0900

    헤더 변경

--- src/views/ContentView.vue
+++ src/views/ContentView.vue
@@ -1,10 +1,5 @@
 <template>
   <div>
-    <router-link :to="{ name: 'SecretClinic' }"
-      >이해건의 은밀한 교습실</router-link
-    >
-    |
-    <router-link :to="{ name: 'TodaysFortune' }">행운의 준용쿠키</router-link>
     <router-view></router-view>
   </div>
 </template>
